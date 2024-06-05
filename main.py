0a87fda869fb4f35a4eaae0d85f2e79f68343f73
Jean-Marie Prevost <jeanmarie.prevost@outlook.com>
2024-06-04 20:33:05 -0400

    Move url test result to new URLTestResult class

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import urllib.request
 import urllib.error
 import time
+from typing import Optional
+from url_test_result import URLTestResult
 
 
-def check_url_through_urllib(url: str) -> None:
-    print("#################################")
+def check_url_through_urllib(url: str) -> URLTestResult:
     print(f'Running a basic test on url "{url}"')
     start_time = time.time()
+    latency: Optional[float] = None
+    status_code: Optional[int] = None
+    error: Optional[str] = None
+    exists: bool = False
 
     try:
         with urllib.request.urlopen(url) as response:
             latency = time.time() - start_time
-            print(f'Results for "{url}":')
-            print(f"URL exists: {response.status == 200}")
-            print(f"Status code: {response.status}")
-            print(f"Latency: {round(latency, 3)}s")
+            exists = response.status == 200
+            status_code = response.status
     except urllib.error.HTTPError as e:
-        print(f"URL does not exist: HTTP Error {e.code}")
+        latency = time.time() - start_time
+        error: Optional[str] = f"HTTP Error {e.code}"
     except ValueError as e:
-        print(f"Error: {e}")
-        if "unknown url type" in str(e):
-            print("(Are you missing a protocol? e.g. https://...)")
+        latency = time.time() - start_time
+        error: Optional[str] = f"Value Error: {e}"
     except urllib.error.URLError as e:
-        if "Errno 11001" in str(e):
-            print(f'Could not resolve hostname "{url}"')
-        else:
-            print(f"Error: {e.reason}")
-    finally:
-        print("#################################")
-        print("")
+        latency = time.time() - start_time
+        error: Optional[str] = f"URL Error: {e}"
+
+    return URLTestResult(url, exists, status_code, latency, error)
 
 
 if __name__ == "__main__":
-    check_url_through_urllib("http://google.com")
-    check_url_through_urllib("google.com")
-    check_url_through_urllib("http://thisurldoesnotexist.whatever")
+    result1 = check_url_through_urllib("http://google.com")
+    result2 = check_url_through_urllib("google.com")
+    result3 = check_url_through_urllib("http://thisurldoesnotexist.whatever")
+
+    result1.print_results()
+    result2.print_results()
+    result3.print_results()
---
+++ url_test_result.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class URLTestResult:
+    def __init__(
+        self,
+        url: str,
+        exists: bool,
+        status_code: Optional[int],
+        latency: Optional[float],
+        error: Optional[str],
+    ):
+        self.url: str = url
+        self.exists: bool = exists
+        self.status_code: Optional[int] = status_code
+        self.latency: Optional[float] = latency
+        self.error: Optional[str] = error
+
+    def __repr__(self) -> str:
+        return (
+            f"URLTestResult(url={self.url}, exists={self.exists}, "
+            f"status_code={self.status_code}, latency={self.latency}, "
+            f"error={self.error})"
+        )
+
+    def print_results(self) -> None:
+        print("#################################")
+        print(f'Results for "{self.url}":')
+        print(f"URL exists: {self.exists}")
+        print(f"Status code: {self.status_code}")
+        print(f"Latency: {round(self.latency, 3)}s")
+
+        if self.error is not None:
+            print("Resulted in an error:")
+            print(self.error)
+            if "unknown url type" in self.error:
+                print("(Hint: Are you missing a protocol? e.g. https://...)")
+            if "Errno 11001" in self.error:
+                print(f'(Hint: Could not resolve hostname "{self.url}")')
+        print("#################################")
+        print("")
