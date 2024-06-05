18587b32ebb0d45b92a53e6dae35c7e45f211a6c
Jean-Marie Prevost <jeanmarie.prevost@outlook.com>
2024-06-05 18:38:22 -0400

    Add systray icon through pystray

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import monitoring
+import system_tray
 
 if __name__ == "__main__":
     result1 = monitoring.check_url_through_urllib("http://google.com")
@@ -8,3 +9,4 @@ if __name__ == "__main__":
     result1.print_results()
     result2.print_results()
     result3.print_results()
+    system_tray.initialize_system_tray()
---
+++ src/system_tray.py
@@ -0,0 +1,39 @@
+"""
+THis module uses pystray to create a system tray icon and allow the program to keep running in the background
+"""
+
+import PIL.Image
+import pystray
+
+
+# Define actions for the RMB menu of the tray icon
+def on_clicked_exit(icon, item):
+    print(f'Clicked "{item}"')
+    print("Going to stop pystray now...")
+    print("Going to exit now...")
+    icon.stop()
+
+
+# Define actions for the RMB menu of the tray icon
+def on_clicked_open_gui(icon, item):
+    print(f'Clicked "{item}"')
+    print("TODO - Implement showing the GUI here")
+    raise NotImplementedError("The GUI or Show GUI function is not yet implemented.")
+
+
+def initialize_system_tray() -> None:
+    # Load the image for the icon
+    image = PIL.Image.open("icon_32px.png")
+
+    # Set up pystray
+    icon = pystray.Icon(
+        "Bump - Web Monitoring Tool",
+        image,
+        menu=pystray.Menu(
+            pystray.MenuItem("Do the thing", on_clicked_open_gui, default=True, visible=False),  # default=True means that this is also the action that gets called when LMB the icon
+            pystray.MenuItem("Exit", on_clicked_exit),
+        ),
+    )
+
+    # Run pystray
+    icon.run()
