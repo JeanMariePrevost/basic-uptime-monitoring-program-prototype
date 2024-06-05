0d1f5a7d1b053b953f4d17a2b8065e4469dcedb4
Jean-Marie Prevost <jeanmarie.prevost@outlook.com>
2024-06-04 20:03:22 -0400

    Change project settings and fix warnings

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import urllib.request
+import urllib.error
 import time
 
 
 def check_url_through_urllib(url: str) -> None:
     print("#################################")
-    print(f"Running a basic test on url \"{url}\"")
+    print(f'Running a basic test on url "{url}"')
     start_time = time.time()
 
     try:
         with urllib.request.urlopen(url) as response:
             latency = time.time() - start_time
-            print(f"Results for \"{url}\":")
+            print(f'Results for "{url}":')
             print(f"URL exists: {response.status == 200}")
             print(f"Status code: {response.status}")
             print(f"Latency: {round(latency, 3)}s")
@@ -18,11 +19,11 @@ def check_url_through_urllib(url: str) -> None:
         print(f"URL does not exist: HTTP Error {e.code}")
     except ValueError as e:
         print(f"Error: {e}")
-        if ("unknown url type" in str(e)):
+        if "unknown url type" in str(e):
             print("(Are you missing a protocol? e.g. https://...)")
     except urllib.error.URLError as e:
-        if (e.reason.errno == 11001):
-            print(f"Could not resolve hostname \"{url}\"")
+        if "Errno 11001" in str(e):
+            print(f'Could not resolve hostname "{url}"')
         else:
             print(f"Error: {e.reason}")
     finally:
@@ -30,7 +31,7 @@ def check_url_through_urllib(url: str) -> None:
         print("")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_url_through_urllib("http://google.com")
     check_url_through_urllib("google.com")
     check_url_through_urllib("http://thisurldoesnotexist.whatever")
