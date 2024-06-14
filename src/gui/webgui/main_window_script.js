822e00ca098a80379dce16b40ad01074454d9cce
Jean-Marie Prevost <jeanmarie.prevost@outlook.com>
2024-06-14 19:46:09 -0400

    Separate the data model from the display in the GUI by itroducing a Monitor data class

--- src/gui/webgui/main_window_script.js
+++ src/gui/webgui/main_window_script.js
@@ -1,3 +1,5 @@
+let _monitors = []; //Array of Monitor data objects
+
 document.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("add-card-btn").addEventListener("click", onAddMonitorClicked);
   document.getElementById("apply-btn").addEventListener("click", updateMonitorsDataInBackend);
@@ -35,30 +37,10 @@ function onAddMonitorClicked() {
  * Sends the data from all monitor cards on the dashboard to the backend
  */
 function updateMonitorsDataInBackend() {
-  const monitorData = serializeMonitorsData();
+  const monitorData = JSON.stringify(_monitors);
   pywebview.api.send_monitors_data_to_backend(monitorData);
 }
 
-/**
- * Serializes the data from all monitor cards on the dashboard to JSON
- */
-function serializeMonitorsData() {
-  const cardContainer = document.getElementById("cards-container");
-  const cards = cardContainer.getElementsByClassName("card");
-
-  const monitors = [];
-  for (let card of cards) {
-    const title = card.querySelector(".monitor-title").textContent;
-    const url = card.querySelector(".monitor-url").textContent;
-    const last_result_status = card.querySelector(".monitor-status").textContent;
-    const last_result_timestamp = card.querySelector(".monitor-last-result-timestamp").textContent;
-    const next_test_timestamp = card.querySelector(".monitor-next-test-timestamp").textContent;
-    const test_interval_in_seconds = parseInt(card.querySelector(".monitor-test-interval").textContent);
-    monitors.push({ title, url, last_result_status, last_result_timestamp, next_test_timestamp, test_interval_in_seconds });
-  }
-
-  return JSON.stringify(monitors);
-}
 
 /**
  * Adds a card existing card element to the dashboard
@@ -72,7 +54,6 @@ function addCardToDashboard(card) {
   showOrHideNoMonitorsCard();
 }
 
-
 /**
  * Creates a new html element for a card monitor
  * @param {string} title - The title of the monitor
@@ -83,7 +64,25 @@ function addCardToDashboard(card) {
  * @param {string} test_interval_in_seconds - The test interval in seconds of the monitor
  * @returns {HTMLDivElement} A new card element for a monitor
  */
-function createCardElement(title, url, last_result_status, last_result_timestamp, next_test_timestamp, test_interval_in_seconds) {
+function createCardElement(monitorDataObject) {
+  let title = monitorDataObject.title;
+  let url = monitorDataObject.url;
+  let last_result_status = monitorDataObject.last_result_status;
+  let last_result_timestamp = monitorDataObject.last_result_timestamp;
+  let next_test_timestamp = monitorDataObject.next_test_timestamp;
+  let test_interval_in_seconds = monitorDataObject.test_interval_in_seconds;
+
+  //Replace null/"falsy" strings with user-friendly display values
+  if (last_result_status === null || last_result_status === "" || last_result_status === "null") {
+    last_result_status = "Status unknown";
+  }
+  if (last_result_timestamp === null || last_result_timestamp === "" || last_result_timestamp === "null") {
+    last_result_timestamp = "Never tested";
+  }
+  if (next_test_timestamp === null || next_test_timestamp === "" || next_test_timestamp === "null") {
+    next_test_timestamp = "No upcoming test scheduled";
+  }
+
   const newCard = document.createElement("div");
   newCard.className = "card";
   newCard.innerHTML = `
@@ -107,17 +106,14 @@ function createCardElement(title, url, last_result_status, last_result_timestamp
   return newCard;
 }
 
-
-
-
 /**
  * Add a listener to the delete button of the card
  * @param {HTMLDivElement} card - The card element
  */
 function addDeleteButtonListener(card) {
   card.querySelector(".delete-btn").addEventListener("click", function () {
-    let parentCard = this.closest('.card');// Get the relevant card element
-    let cardTitle = parentCard.querySelector('.monitor-title').textContent;
+    let parentCard = this.closest(".card"); // Get the relevant card element
+    let cardTitle = parentCard.querySelector(".monitor-title").textContent;
     const confirmed = confirm(`Are you sure you want to delete the following monitor?\n"${cardTitle}"\n\nThis action cannot be undone.`);
     if (confirmed) {
       animateAndRemoveCard(parentCard);
@@ -132,22 +128,27 @@ function addDeleteButtonListener(card) {
 function addEditButtonListener(card) {
   card.querySelector(".edit-btn").addEventListener("click", function () {
     // Get the card's current data
-    const card = this.parentNode;
-    const cardTitle = card.querySelector(".monitor-title");
-    const cardURL = card.querySelector(".monitor-url");
-    const cardStatus = card.querySelector(".monitor-status");
+    const title = card.querySelector(".monitor-title");
+    const url = card.querySelector(".monitor-url");
+    const testInterval = card.querySelector(".monitor-test-interval");
 
     //Basic prompt to edit the card
-    const newTitle = prompt("Enter the new title", cardTitle.textContent);
-    const newURL = prompt("Enter the new details", cardURL.textContent);
-    const newStatus = prompt("DEBUG - Enter a status", cardStatus.textContent);
+    const newTitle = prompt("Enter the new title", title.textContent);
+    const newURL = prompt("Enter the new details", url.textContent);
+    const newTestInterval = prompt("Enter the new test interval", testInterval.textContent);
 
+    //Update the Monitor object in the array
+    const monitor = getMonitorByTitle(title.textContent);
+    monitor.title = newTitle;
+    monitor.url = newURL;
+    monitor.test_interval_in_seconds = newTestInterval;
+    
     // Update the card with the new data
-    if (newTitle && newURL && newStatus) {
-      cardTitle.textContent = newTitle;
-      cardURL.href = newURL;
-      cardURL.textContent = newURL;
-      cardStatus.textContent = newStatus;
+    if (newTitle && newURL && newTestInterval) {
+      title.textContent = newTitle;
+      url.href = newURL;
+      url.textContent = newURL;
+      testInterval.textContent = newTestInterval;
     }
   });
 }
@@ -166,7 +167,7 @@ function animateAndRemoveCard(card) {
   card.addEventListener("animationend", () => {
     console.log("Card removal animation ended");
     card.remove();
-    updateMonitorsDataInBackend();//Apply the changes to the backend immediately
+    updateMonitorsDataInBackend(); //Apply the changes to the backend immediately
     showOrHideNoMonitorsCard();
   });
 }
@@ -179,7 +180,7 @@ function showOrHideNoMonitorsCard() {
   const noMonitorsCard = document.getElementById("no-monitors-card");
   const otherCards = cardContainer.getElementsByClassName("card");
 
-  console.log(`Number of cards: ${otherCards.length}`)
+  console.log(`Number of cards: ${otherCards.length}`);
 
   if (otherCards.length === 0) {
     noMonitorsCard.style.display = "block";
@@ -188,17 +189,26 @@ function showOrHideNoMonitorsCard() {
   }
 }
 
-
 /**
  * Hook for receiving monitor data from the backend
- * @param {string} jsonMonitorsData - The JSON string of monitor data 
+ * @param {string} jsonMonitorsData - The JSON string of monitor data
  */
 function receiveMonitorsDataFromBackend(jsonMonitorsData) {
+  //DEBUG
   console.log("Received monitor data from backend:");
   console.log(jsonMonitorsData);
-  const monitorsData = JSON.parse(jsonMonitorsData);
 
-  //Rough refresh method: Remove all cards from cards-container, EXCLUDING the no-monitors-card element
+  //Update the data
+  updateMonitorsArray(jsonMonitorsData);
+
+  //Rebuild the visuals
+  refreshMonitorCards();
+}
+
+/**
+ * Removes all monitor card elements from the dashboard
+ */
+function removeAllMonitorCardElements() {
   const cardContainer = document.getElementById("cards-container");
   const cards = cardContainer.getElementsByClassName("card");
   for (let i = cards.length - 1; i >= 0; i--) {
@@ -207,20 +217,57 @@ function receiveMonitorsDataFromBackend(jsonMonitorsData) {
       card.remove();
     }
   }
+}
 
-  
-
-  for (let monitor of monitorsData) {
-    console.log(`title: ${monitor.title}`);
-    console.log(`url: ${monitor.url}`);
-    console.log(`last_result_status: ${monitor.last_result_status}`);
-    console.log(`last_result_timestamp: ${monitor.last_result_timestamp}`);
-    console.log(`next_test_timestamp: ${monitor.next_test_timestamp}`);
-    console.log(`test_interval_in_seconds: ${monitor.test_interval_in_seconds}`);
-
-    newMonitorCard = createCardElement(monitor.title, monitor.url, monitor.last_result_status, monitor.last_result_timestamp, monitor.next_test_timestamp, monitor.test_interval_in_seconds)
+/**
+ * Rebuilds the monitor cards from the current monitor data array
+ */
+function refreshMonitorCards() {
+  removeAllMonitorCardElements();
+  for (let monitor of _monitors) {
+    const newMonitorCard = createCardElement(monitor);
     addCardToDashboard(newMonitorCard);
   }
 }
 
+/**
+ * Rebuilds the array of monitors data objects from a JSON string
+ * @param {string} jsonMonitorsData - The JSON string of monitor data
+ */
+function updateMonitorsArray(jsonMonitorsData) {
+  const monitorsData = JSON.parse(jsonMonitorsData);
+  _monitors = monitorsData.map(
+    (monitor) => new Monitor(monitor.title, monitor.url, monitor.last_result_status, monitor.last_result_timestamp, monitor.next_test_timestamp, monitor.test_interval_in_seconds)
+  );
+}
 
+/**
+ * Monitor data object to hold the data separate from their visual representation
+ */
+class Monitor {
+  /**
+   * Create a new Monitor object
+   * @param {string} title - The title of the monitor
+   * @param {string} url - The URL of the monitor
+   * @param {string} last_result_status - The last result status of the monitor
+   * @param {string} last_result_timestamp - The last result timestamp of the monitor
+   * @param {string} next_test_timestamp - The next test timestamp of the monitor
+   * @param {string} test_interval_in_seconds - The test interval in seconds of the monitor
+   */
+  constructor(title, url, last_result_status, last_result_timestamp, next_test_timestamp, test_interval_in_seconds) {
+    this.title = title;
+    this.url = url;
+    this.last_result_status = last_result_status;
+    this.last_result_timestamp = last_result_timestamp;
+    this.next_test_timestamp = next_test_timestamp;
+    this.test_interval_in_seconds = test_interval_in_seconds;
+  }
+}
+
+/**
+ * Get the Monitor object from the array of monitors by its title
+ * @param {string} title - The title of the monitor
+ */
+function getMonitorByTitle(title) {
+  return _monitors.find((monitor) => monitor.title === title);
+}
